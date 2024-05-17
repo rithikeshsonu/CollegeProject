92ed878ce0a67d2a6d2bda4a48991d540b2aa101
rithikeshsurthi <rithikeshsonu13@gmail.com>
2024-05-17 23:27:01 +0530

    Installed Serilog and implemented 'Log to file' using Serilog.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 using CollegeProject.MyLogging;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-
+/*
 builder.Logging.ClearProviders(); //Removes all types of logging mechanisms from application
 builder.Logging.AddConsole(); //Logs only to console
 builder.Logging.AddDebug();//Logs only to debug
+*/
+
+//Using SeriLog
+//logs to file - with minimum level as Information. Log will be generated with time gap as specified.(Minutes, days, Hours, etc.,)
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.File("Log/log.txt",rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+//builder.Host.UseSerilog(); //Only to use Serilog by overriding existing loggers.
+builder.Logging.AddSerilog(); // To add Serilog logging with other type of existing loggings as well.
 
 // Add services to the container.
 //Content Negotiation -> How to now allow other response types like xml, bla bla...
