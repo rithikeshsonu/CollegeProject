f72512f5530144a859c43e6f6d7d4d0241df7f5e
rithikeshsurthi <rithikeshsonu13@gmail.com>
2024-05-18 15:16:50 +0530

    Installed Entity Framework packages and created Class file and DBContext file for Code First approach.

---
+++ Data/CollegeDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CollegeProject.Data
+{
+    public class CollegeDBContext : DbContext
+    {
+        DbSet<Student> Students { get; set; }
+    }
+}
