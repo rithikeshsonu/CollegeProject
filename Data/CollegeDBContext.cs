d08ce6f3f441feb68e1fb95eff1ad60ee7fc3cea
rithikeshsurthi <rithikeshsonu13@gmail.com>
2024-05-18 19:19:11 +0530

    Inserted two rows in Students table using model overriding.

--- Data/CollegeDBContext.cs
+++ Data/CollegeDBContext.cs
@@ -9,5 +9,28 @@ namespace CollegeProject.Data
 
         }
         DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(new List<Student>() 
+            {
+                new Student
+                {
+                    StudentID = 1,
+                    StudentName = "Sample1",
+                    Address = "India",
+                    Email = "india@gmail.com",
+                    DOB = new DateTime(2008, 5, 12)
+                },
+                new Student
+                {
+                    StudentID = 2,
+                    StudentName = "Sample2",
+                    Address = "USA",
+                    Email = "usa@gmail.com",
+                    DOB = new DateTime(2012, 3, 8)
+                }
+            });
+        }
     }
 }
---
+++ Migrations/20240518134633_AddDataToStudentsTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CollegeProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataToStudentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentID", "Address", "DOB", "Email", "StudentName" },
+                values: new object[,]
+                {
+                    { 1, "India", new DateTime(2008, 5, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "india@gmail.com", "Sample1" },
+                    { 2, "USA", new DateTime(2012, 3, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), "usa@gmail.com", "Sample2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentID",
+                keyValue: 2);
+        }
+    }
+}
